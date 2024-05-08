9cc7448b319c56f884133dde1b1899b1812036be
alephao <7674479+alephao@users.noreply.github.com>
2024-05-08 09:15:17 -0300

    feat: add <| and <<< operators

--- Sources/Plumbing/Operators+Compose.swift
+++ Sources/Plumbing/Operators+Compose.swift
@@ -2,6 +2,14 @@ public func >>> <A, B, C> (_ a2b: @escaping (A) -> B, _ b2c: @escaping (B) -> C)
   { a in b2c(a2b(a)) }
 }
 
+public func <<< <A, B, C>(_ b2c: @escaping (B) -> C, _ a2b: @escaping (A) -> B) -> (A) -> C {
+  return { a in b2c(a2b(a)) }
+}
+
 public func >>> <A, B, C> (_ a2b: @escaping (A) async -> B, _ b2c: @escaping (B) async -> C) -> (A) async -> C {
   { a in await b2c(a2b(a))  }
+}
+
+public func <<< <A, B, C>(_ b2c: @escaping (B) async -> C, _ a2b: @escaping (A) async -> B) -> (A) async -> C {
+  return { a in await b2c(a2b(a)) }
 }
--- Sources/Plumbing/Operators+Pipe.swift
+++ Sources/Plumbing/Operators+Pipe.swift
@@ -2,6 +2,14 @@ public func |> <A, B> (_ a: A, f: (A) -> B) -> B {
   f(a)
 }
 
+public func <| <A, B> (f: (A) -> B, a: A) -> B {
+  f(a)
+}
+
 public func |> <A, B> (_ a: A, f: (A) async -> B) async -> B {
+  await f(a)
+}
+
+public func <| <A, B> (f: (A) async -> B, a: A) async -> B {
   await f(a)
 }
