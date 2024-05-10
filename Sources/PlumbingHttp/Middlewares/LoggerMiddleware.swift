7501f7b304b2f134ca31baad535dff3ff94e7066
alephao <7674479+alephao@users.noreply.github.com>
2024-05-10 09:44:04 -0300

    feat: update loggerMiddleware request time display

--- Sources/PlumbingHttp/Middlewares/LoggerMiddleware.swift
+++ Sources/PlumbingHttp/Middlewares/LoggerMiddleware.swift
@@ -23,14 +23,36 @@ public func loggerMiddleware(logger: Logger) -> PlumbingHTTPMiddleware {
       )
       let res = await next()
       let endTime = Date().timeIntervalSince1970
+      let timeDiff = formatSecondsToDisplay(endTime - startTime)
+
       logger.log(
         level: .info,
         """
         \(requestID) [Res] \(res.status.code) \
-        in \(Int((endTime - startTime) * 1000))ms
+        in \(timeDiff)
         """
       )
       return res
     }
   }
+}
+
+private func formatSecondsToDisplay(_ timeInterval: TimeInterval) -> String {
+  if timeInterval >= 1 {
+    return "\(timeInterval)s"
+  }
+
+  if timeInterval >= 0.001 {
+    return "\(Int(timeInterval * 1_000))ms"
+  }
+
+  if timeInterval >= 0.000001 {
+    return "\(Int(timeInterval * 1_000_000))µs"
+  }
+
+  if timeInterval >= 0.000000001 {
+    return "\(Int(timeInterval * 1_000_000))µs"
+  }
+
+  return "\(Int(timeInterval * 1_000_000_000))ns"
 }
