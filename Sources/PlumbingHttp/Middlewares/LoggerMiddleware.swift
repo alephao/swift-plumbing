fc3be0cd742b56bca4d17224af93eab8b2b4b054
alephao <7674479+alephao@users.noreply.github.com>
2024-05-10 09:51:20 -0300

    feat: single log entry with loggerMiddleware

--- Sources/PlumbingHttp/Middlewares/LoggerMiddleware.swift
+++ Sources/PlumbingHttp/Middlewares/LoggerMiddleware.swift
@@ -14,13 +14,11 @@ public func loggerMiddleware(logger: Logger) -> PlumbingHTTPMiddleware {
       // FIXME: Make UUID and Date mockable
       let requestID = UUID()
       let startTime = Date().timeIntervalSince1970
-      logger.log(
-        level: .info,
-        """
-        \(requestID) [Req] \(req.method) \
-        \(req.uri.path)
-        """
-      )
+      // FIXME: Trying a single log entry instead of 1 req + 1 res log
+      // This can cause some issues, like if the task never ends, we will never
+      // know it started in the first place.
+      // So I guess we gotta figure out how to cancel this on timeout and log that it timmed out.
+      // Maybe we can just use a Task, but I will check later
       let res = await next()
       let endTime = Date().timeIntervalSince1970
       let timeDiff = formatSecondsToDisplay(endTime - startTime)
@@ -28,8 +26,10 @@ public func loggerMiddleware(logger: Logger) -> PlumbingHTTPMiddleware {
       logger.log(
         level: .info,
         """
-        \(requestID) [Res] \(res.status.code) \
-        in \(timeDiff)
+        \(requestID) \
+        \(res.status.code) \
+        in \(timeDiff) \
+        \(req.uri.path)
         """
       )
       return res
