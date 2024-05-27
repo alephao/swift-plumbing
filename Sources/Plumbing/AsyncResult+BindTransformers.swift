4f6bfdc77d46692a75ed064c48dbe7c031f4b75c
alephao <7674479+alephao@users.noreply.github.com>
2024-05-27 16:11:37 -0300

    feat: extract bind functions

--- Sources/Plumbing/AsyncResult+BindTransformers.swift
+++ Sources/Plumbing/AsyncResult+BindTransformers.swift
@@ -1,1 +1,197 @@
-public enum Bind {}
+import Dependencies
+import Prelude
+
+public enum Bind {
+  public static func middleware<Success, Failure>(
+    run other: @escaping (Success) -> AsyncResult<Success, Failure>,
+    if predicate: @escaping (Success) -> Bool
+  ) -> (Success) -> AsyncResult<Success, Failure> {
+    { success in
+      if predicate(success) {
+        return other(success)
+      }
+      return .success(success)
+    }
+  }
+
+  public static func ensure<Success, Failure>(
+    _ predicate: @escaping (Success) -> Bool,
+    orFail fail: @escaping (Success) -> Failure
+  ) -> (Success) -> AsyncResult<Success, Failure> {
+    middleware(run: fail >>> AsyncResult.failure, if: not <<< predicate)
+  }
+
+  public static func runAndWait<Success, Failure>(
+    _ task: @escaping (Success) async -> Void
+  ) -> (Success) -> AsyncResult<Success, Failure> {
+    { success in
+      .init {
+        await task(success)
+        return .success(success)
+      }
+    }
+  }
+
+  public static func fireAndForget<Success, Failure>(
+    _ task: @escaping (Success) async -> Void
+  ) -> (Success) -> AsyncResult<Success, Failure> {
+    { success in
+      .init {
+        @Dependency(\.fireAndForget) var fnf
+        await fnf {
+          await task(success)
+        }
+        return .success(success)
+      }
+    }
+  }
+
+  public static func prepend<Success, Failure, OtherSuccess>(
+    _ other: @escaping (Success) -> AsyncResult<OtherSuccess, Failure>
+  ) -> (Success) -> AsyncResult<T2<OtherSuccess, Success>, Failure> {
+    { success in
+      other(success).map({ otherSuccess in otherSuccess .*. success })
+    }
+  }
+
+  public static func prepend<Success, Failure, OtherSuccess>(
+    _ other: @escaping (Success) -> AsyncResult<OtherSuccess?, Failure>,
+    orFail fail: @escaping (Success) -> Failure
+  ) -> (Success) -> AsyncResult<T2<OtherSuccess, Success>, Failure> {
+    { success in
+      other(success)
+        .flatMap({ otherSuccess in
+          switch otherSuccess {
+          case .none:
+            return .failure(fail(success))
+          case let .some(otherSuccessUnwrapped):
+            return .success(otherSuccessUnwrapped .*. success)
+          }
+        })
+    }
+  }
+
+  public static func fork<Success, Failure, NewSuccess>(
+    predicate: @escaping (Success) -> Bool,
+    ifTrue: @escaping (Success) -> AsyncResult<NewSuccess, Failure>,
+    ifFalse: @escaping (Success) -> AsyncResult<NewSuccess, Failure>
+  ) -> (Success) -> AsyncResult<NewSuccess, Failure> {
+    { success in
+      return predicate(success)
+        ? ifTrue(success)
+        : ifFalse(success)
+    }
+  }
+
+  public static func `switch`<Success>(
+    to other: @escaping (Success) -> AsyncResult<Success, Success>,
+    if predicate: @escaping (Success) -> Bool
+  ) -> (Success) -> AsyncResult<Success, Success> {
+    { success in
+      if predicate(success) {
+        return AsyncResult {
+          let res = await other(success).run()
+          return .failure(res.either())
+        }
+      }
+      return .success(success)
+    }
+  }
+
+  public static func unwrap<Wrapped, Failure>(
+    orFail fail: @escaping () -> Failure
+  ) -> (Wrapped?) -> AsyncResult<Wrapped, Failure> {
+    { wrapped in
+      if let unwrapped = wrapped {
+        return .success(unwrapped)
+      }
+      return .failure(fail())
+    }
+  }
+
+  public static func unwrap<Wrapped, Failure>(
+    or other: @escaping () -> AsyncResult<Wrapped, Failure>
+  ) -> (Wrapped?) -> AsyncResult<Wrapped, Failure> {
+    { wrapped in
+      if let unwrapped = wrapped {
+        return .success(unwrapped)
+      }
+      return other()
+    }
+  }
+
+  public static func unwrap<Success, Failure, Property>(
+    property: @escaping (Success) -> Property?,
+    orFail fail: @escaping (Success) -> Failure
+  ) -> (Success) -> AsyncResult<T2<Property, Success>, Failure> {
+    { success in
+      if let unwrapped = property(success) {
+        return .success(unwrapped .*. success)
+      }
+      return .failure(fail(success))
+    }
+  }
+
+  public static func parallel<Success, Failure, A, B>(
+    a: @escaping (Success) -> AsyncResult<A, Failure>,
+    b: @escaping (Success) -> AsyncResult<B, Failure>
+  ) -> (Success) -> AsyncResult<T3<Success, A, B>, Failure> {
+    { success in
+      AsyncResult {
+        // TODO: Do not wait for both if one fails first
+        async let _a = a(success).run()
+        async let _b = b(success).run()
+        let (resA, resB) = await (_a, _b)
+        return
+          resB
+          .prepend(resA)
+          .prepend(success)
+      }
+    }
+  }
+
+  public static func parallel<Success, Failure, A, B, C>(
+    a: @escaping (Success) -> AsyncResult<A, Failure>,
+    b: @escaping (Success) -> AsyncResult<B, Failure>,
+    c: @escaping (Success) -> AsyncResult<C, Failure>
+  ) -> (Success) -> AsyncResult<T4<Success, A, B, C>, Failure> {
+    { success in
+      AsyncResult {
+        // TODO: Do not wait for both if one fails first
+        async let _a = a(success).run()
+        async let _b = b(success).run()
+        async let _c = c(success).run()
+        let (resA, resB, resC) = await (_a, _b, _c)
+        return
+          resC
+          .prepend(resB)
+          .prepend(resA)
+          .prepend(success)
+      }
+    }
+  }
+
+  public static func parallel<Success, Failure, A, B, C, D>(
+    a: @escaping (Success) -> AsyncResult<A, Failure>,
+    b: @escaping (Success) -> AsyncResult<B, Failure>,
+    c: @escaping (Success) -> AsyncResult<C, Failure>,
+    d: @escaping (Success) -> AsyncResult<D, Failure>
+  ) -> (Success) -> AsyncResult<T5<Success, A, B, C, D>, Failure> {
+    { success in
+      AsyncResult {
+        // TODO: Do not wait for both if one fails first
+        async let _a = a(success).run()
+        async let _b = b(success).run()
+        async let _c = c(success).run()
+        async let _d = d(success).run()
+        let (resA, resB, resC, resD) = await (_a, _b, _c, _d)
+        return
+          resD
+          .prepend(resC)
+          .prepend(resB)
+          .prepend(resA)
+          .prepend(success)
+      }
+    }
+  }
+}
--- Sources/Plumbing/AsyncResult+Builders.swift
+++ Sources/Plumbing/AsyncResult+Builders.swift
@@ -41,12 +41,7 @@ extension AsyncResult {
     run other: @escaping (Success) -> AsyncResult<Success, Failure>,
     if predicate: @escaping (Success) -> Bool
   ) -> Self {
-    self.flatMap({ success in
-      if predicate(success) {
-        return other(success)
-      }
-      return .success(success)
-    })
+    self.flatMap(Bind.middleware(run: other, if: predicate))
   }
 
   /// Fail with the provided closure if the predicate is not true
@@ -57,7 +52,7 @@ extension AsyncResult {
     _ predicate: @escaping (Success) -> Bool,
     orFail fail: @escaping (Success) -> Failure
   ) -> Self {
-    middleware(run: fail >>> Self.failure, if: not <<< predicate)
+    self.flatMap(Bind.ensure(predicate, orFail: fail))
   }
 
   /// Run a non-failable effect and wait for it to finish
@@ -67,12 +62,7 @@ extension AsyncResult {
   public func runAndWait(
     _ task: @escaping (Success) async -> Void
   ) -> Self {
-    self.flatMap({ success in
-      .init {
-        await task(success)
-        return .success(success)
-      }
-    })
+    self.flatMap(Bind.runAndWait(task))
   }
 
   /// Fire an effect and forget about it
@@ -82,15 +72,7 @@ extension AsyncResult {
   public func fireAndForget(
     _ task: @escaping (Success) async -> Void
   ) -> Self {
-    self.flatMap({ success in
-      .init {
-        @Dependency(\.fireAndForget) var fnf
-        await fnf {
-          await task(success)
-        }
-        return .success(success)
-      }
-    })
+    self.flatMap(Bind.fireAndForget(task))
   }
 }
 
@@ -103,9 +85,7 @@ extension AsyncResult {
   public func prepend<OtherSuccess>(
     _ other: @escaping (Success) -> AsyncResult<OtherSuccess, Failure>
   ) -> AsyncResult<T2<OtherSuccess, Success>, Failure> {
-    self.flatMap({ success in
-      other(success).map({ otherSuccess in otherSuccess .*. success })
-    })
+    self.flatMap(Bind.prepend(other))
   }
 
   /// Run other and prepend the result if the value is not nil
@@ -116,17 +96,7 @@ extension AsyncResult {
     _ other: @escaping (Success) -> AsyncResult<OtherSuccess?, Failure>,
     orFail fail: @escaping (Success) -> Failure
   ) -> AsyncResult<T2<OtherSuccess, Success>, Failure> {
-    self.flatMap({ success in
-      other(success)
-        .flatMap({ otherSuccess in
-          switch otherSuccess {
-          case .none:
-            return .failure(fail(success))
-          case let .some(otherSuccessUnwrapped):
-            return .success(otherSuccessUnwrapped .*. success)
-          }
-        })
-    })
+    self.flatMap(Bind.prepend(other, orFail: fail))
   }
 }
 
@@ -142,11 +112,7 @@ extension AsyncResult {
     ifTrue: @escaping (Success) -> AsyncResult<NewSuccess, Failure>,
     ifFalse: @escaping (Success) -> AsyncResult<NewSuccess, Failure>
   ) -> AsyncResult<NewSuccess, Failure> {
-    self.flatMap({ success in
-      return predicate(success)
-        ? ifTrue(success)
-        : ifFalse(success)
-    })
+    self.flatMap(Bind.fork(predicate: predicate, ifTrue: ifTrue, ifFalse: ifFalse))
   }
 
   /// Switch to a different pipeline
@@ -159,15 +125,7 @@ extension AsyncResult {
     to other: @escaping (Success) -> AsyncResult<Success, Failure>,
     if predicate: @escaping (Success) -> Bool
   ) -> Self where Success == Failure {
-    .init {
-      switch await run() {
-      case let .success(success):
-        return predicate(success)
-          ? .failure(success)
-          : .success(success)
-      case let .failure(failure): return .failure(failure)
-      }
-    }
+    self.flatMap(Bind.switch(to: other, if: predicate))
   }
 }
 
@@ -182,12 +140,7 @@ extension AsyncResult {
   public func unwrap<Wrapped>(
     orFail fail: @escaping () -> Failure
   ) -> AsyncResult<Wrapped, Failure> where Success == Wrapped? {
-    self.flatMap({ wrapped in
-      if let unwrapped = wrapped {
-        return .success(unwrapped)
-      }
-      return .failure(fail())
-    })
+    self.flatMap(Bind.unwrap(orFail: fail))
   }
 
   /// Unwraps an optional or run a pipeline that returns an unwrapped value
@@ -199,12 +152,7 @@ extension AsyncResult {
   public func unwrap<Wrapped>(
     or other: @escaping () -> AsyncResult<Wrapped, Failure>
   ) -> AsyncResult<Wrapped, Failure> where Success == Wrapped? {
-    self.flatMap({ wrapped in
-      if let unwrapped = wrapped {
-        return .success(unwrapped)
-      }
-      return other()
-    })
+    self.flatMap(Bind.unwrap(or: other))
   }
 
   /// Unwraps an optional property or fails with the provided closure
@@ -215,12 +163,7 @@ extension AsyncResult {
     property: @escaping (Success) -> Property?,
     orFail fail: @escaping (Success) -> Failure
   ) -> AsyncResult<T2<Property, Success>, Failure> {
-    self.flatMap({ success in
-      if let unwrapped = property(success) {
-        return .success(unwrapped .*. success)
-      }
-      return .failure(fail(success))
-    })
+    self.flatMap(Bind.unwrap(property: property, orFail: fail))
   }
 }
 
@@ -229,62 +172,34 @@ extension AsyncResult {
   /// Run a and b in parallel
   ///
   ///-      ┏━━[A]━━┓
-  /// ━[S]━━╋━━━━━━━╋━[(S,A,B)]━━▶
+  /// ━[S]━━╋━━━━━━━╋━[S,A,B]━━▶
   ///       ┗━━[B]━━┛
   public func parallel<A, B>(
     a: @escaping (Success) -> AsyncResult<A, Failure>,
     b: @escaping (Success) -> AsyncResult<B, Failure>
   ) -> AsyncResult<T3<Success, A, B>, Failure> {
-    .init {
-      switch await run() {
-      case let .success(success):
-        // TODO: Do not wait for both if one fails first
-        async let _a = a(success).run()
-        async let _b = b(success).run()
-        let (resA, resB) = await (_a, _b)
-        return
-          resB
-          .prepend(resA)
-          .prepend(success)
-      case let .failure(failure): return .failure(failure)
-      }
-    }
+    self.flatMap(Bind.parallel(a: a, b: b))
   }
 
   /// Run a, b, and c in parallel
   ///
   ///-      ┏━━[A]━━┓
   ///       ┣━━[B]━━┫
-  /// ━[S]━━╋━━━━━━━╋━[t(S,A,B,C)]━━▶
+  /// ━[S]━━╋━━━━━━━╋━[S,A,B,C]━━▶
   ///       ┗━━[C]━━┛
   public func parallel<A, B, C>(
     a: @escaping (Success) -> AsyncResult<A, Failure>,
     b: @escaping (Success) -> AsyncResult<B, Failure>,
     c: @escaping (Success) -> AsyncResult<C, Failure>
   ) -> AsyncResult<T4<Success, A, B, C>, Failure> {
-    .init {
-      switch await run() {
-      case let .success(success):
-        // TODO: Do not wait for both if one fails first
-        async let _a = a(success).run()
-        async let _b = b(success).run()
-        async let _c = c(success).run()
-        let (resA, resB, resC) = await (_a, _b, _c)
-        return
-          resC
-          .prepend(resB)
-          .prepend(resA)
-          .prepend(success)
-      case let .failure(failure): return .failure(failure)
-      }
-    }
+    self.flatMap(Bind.parallel(a: a, b: b, c: c))
   }
 
   /// Run a, b, c, and d in parallel
   ///
   ///-      ┏━━[A]━━┓
   ///       ┣━━[B]━━┫
-  /// ━[S]━━╋━━━━━━━╋━[t(S,A,B,C,D)]━━▶
+  /// ━[S]━━╋━━━━━━━╋━[S,A,B,C,D]━━▶
   ///       ┣━━[C]━━┫
   ///       ┗━━[D]━━┛
   public func parallel<A, B, C, D>(
@@ -293,23 +208,6 @@ extension AsyncResult {
     c: @escaping (Success) -> AsyncResult<C, Failure>,
     d: @escaping (Success) -> AsyncResult<D, Failure>
   ) -> AsyncResult<T5<Success, A, B, C, D>, Failure> {
-    .init {
-      switch await run() {
-      case let .success(success):
-        // TODO: Do not wait for both if one fails first
-        async let _a = a(success).run()
-        async let _b = b(success).run()
-        async let _c = c(success).run()
-        async let _d = d(success).run()
-        let (resA, resB, resC, resD) = await (_a, _b, _c, _d)
-        return
-          resD
-          .prepend(resC)
-          .prepend(resB)
-          .prepend(resA)
-          .prepend(success)
-      case let .failure(failure): return .failure(failure)
-      }
-    }
+    self.flatMap(Bind.parallel(a: a, b: b, c: c, d: d))
   }
 }
