7d5550f9077a9fbf9fb95b4b508d855747222f88
alephao <7674479+alephao@users.noreply.github.com>
2024-06-14 00:19:00 -0300

    InjectIII

--- Template/Sources/Server/Server.swift
+++ Template/Sources/Server/Server.swift
@@ -1,9 +1,18 @@
 import Application
 import ArgumentParser
 
+#if os(macOS) && DEBUG
+  import Foundation
+#endif
+
 @main
 struct Server: AsyncParsableCommand {
   func run() async throws {
+    #if os(macOS) && DEBUG
+      Bundle(path: "/Applications/InjectionIII.app/Contents/Resources/macOSInjection.bundle")?
+        .load()
+    #endif
+
     let app = buildApplication()
     try await app.runService()
   }
